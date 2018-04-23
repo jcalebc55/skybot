3e84e4f291bdb65da933cccdb6df003be8a3137f
caleb <caleb@caleb-ThinkPad-T420>
2018-04-23 15:29:12 -0700

    Initial Commit

--- src/com/nwctarobotics/SkybotScoringSoftware/MatchPosition.java
+++
@@ -1,5 +0,0 @@
-package com.nwctarobotics.SkybotScoringSoftware;
-
-public enum MatchPosition {
-    Blue1, Blue2, Red1, Red2;
-}
--- src/com/nwctarobotics/SkybotScoringSoftware/sql/options/DatabaseOptions.java
+++
@@ -1,9 +0,0 @@
-package com.nwctarobotics.SkybotScoringSoftware.sql.options;
-
-/**
- * Used just to identify options
- * 
- * @author mike101102
- */
-public interface DatabaseOptions {
-}
